3af6d73d0f1f8fac6702539ebbe87b093c7592a9
danilohumberto <danilohumberto12@hotmail.com>
2023-12-10 13:24:04 -0300

    result3atv

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
-app.use(express.static("public"))
+//app.use(express.static("public")) - usado para chamar o html
 const bodyParser = require("body-parser")
 var urlencodedParser = bodyParser.urlencoded({extended:false})
 
+const { Op } = require('sequelize')
 const informacoes = require('./model/cadastrar')
 //informacoes.sync()
 
-app.post('/addCar', urlencodedParser, (req, res) => { 
+// sincronizando com a pasta view
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+
+    res.render('home')
+})
+
+app.get('/cadastrarCarro', (req, res) => {
+
+    res.render('cadastrarCarro')
+
+})
+
+app.post('/addCar', urlencodedParser, (req,res) => {
 
     var marca = req.body.marca
     var nomeCarro = req.body.nomeCarro
@@ -16,23 +32,119 @@ app.post('/addCar', urlencodedParser, (req, res) => {
     var nomeDono = req.body.nomeDono
     var cpf = req.body.cpf
 
-    var carros = []
+    // inserindo os dados no banco de dados
 
-    var newsCar = {marca:marca, nomeCarro:nomeCarro, anoFabric:anoFabric, useOrZero:useOrZero, nomeDono:nomeDono, cpf:cpf}
-    carros.push(newsCar)
-
-    var fullAnswer = ''
-    for(var i = 0; i < carros.length; i++){
-        fullAnswer += '<div>'
-        fullAnswer += 'Marca: ' + carros[i].marca + '<br>'
-        fullAnswer += 'Nome do carro: ' + carros[i].nomeCarro + '<br>'
-        fullAnswer += 'Ano de Fabricação: ' + carros[i].anoFabric + '<br>'
-        fullAnswer += 'Carro usado ou Carro zero? ' + carros[i].useOrZero + '<br>'
-        fullAnswer += 'Nome do dono: ' + carros[i].nomeDono + '<br>'
-        fullAnswer += 'Cpf do dono: ' + carros[i].cpf + '<br>'
-        fullAnswer += '</div>'
-    }
-    res.send(fullAnswer)
+    var info = informacoes.create({
+        marca:marca,
+        nomeCarro:nomeCarro,
+        anoFabric:anoFabric,
+        useOrZero:useOrZero,
+        nomeDono:nomeDono,
+        cpf:cpf
+    }).then(() => {
+        res.send('Produto inserido com sucesso.')
+    }).catch((erro) => {
+        res.send('Erro ao inserir o produto: ' + erro)
+    })
 })
+
+app.get('/buscaCarros', (req, res) => {
+
+    res.render('buscaCarros')
+})
+
+app.post('/resultadoBusca', urlencodedParser, (req, res) => {
+
+    var buscaMarca = req.body.marca
+    var buscaNome = req.body.nomeCarro
+    var buscaAno = req.body.anoFabric
+
+    var marcaFiltro = `%${buscaMarca}%`
+    var nomeFiltro = `%${buscaNome}%`
+    
+    informacoes.findAll({
+        where: {
+            marca: { [Op.like]: marcaFiltro},
+            nomeCarro: { [Op.like]: nomeFiltro}
+        }
+
+    }).then((carros) => {
+        res.render('resultSearch', {carros:carros})
+    }).catch((erro) => {
+        res.send('Erro: ' + erro)
+    })
+})
+
+app.get('/atualizarCarros', (req, res) => {
+
+    var idCarro = req.query.id
+
+    informacoes.findOne({
+        where : {
+            id: idCarro
+        }
+    }).then((carros) => {
+        var formulario = `
+            <form action="/updateCarros" method="post">
+                <input type='hidden' name='idUp' value='${carros.id}'><br>
+                Marca: <input type="text" name="marcaUp" value='${carros.marca}'><br>
+                Nome do Carro: <input type="text" name="nomeUp" value='${carros.nomeCarro}'><br>
+                Ano de Fabricação: <input type="text" name="anoUp" value='${carros.anoFabric}'><br>
+                Usado ou Carro Zero? <input type="text" name="useOrZeroUp" value='${carros.useOrZero}'><br>
+                Nome do Dono: <input type="text" name="nomeDonoUp" value='${carros.nomeDono}'><br>
+                Cpf do Dono: <input type="text" name="cpfUp" value='${carros.cpf}'><br>
+                <input type="submit" value="enviar">
+            </form>
+        `
+
+        res.send(formulario)
+    }).catch((erro) => {
+        res.send('Erro: ' + erro)
+    })
+})
+
+app.post('/updateCarros', urlencodedParser, (req, res) => {
+
+    let idUp = req.body.idUp
+    let marcaUp = req.body.marcaUp
+    let nomeUp = req.body.nomeUp
+    let anoUp = req.body.anoUp
+    let useOrZeroUp = req.body.useOrZeroUp
+    let nomeDonoUp = req.body.nomeDonoUp
+    let cpfUp = req.body.cpfUp
+
+    informacoes.update({
+        marca: marcaUp,
+        nomeCarro: nomeUp,
+        anoFabric: anoUp,
+        useOrZero: useOrZeroUp,
+        nomeDono: nomeDonoUp,
+        cpf: cpfUp
+    } , {
+        where: {
+            id: idUp
+        }
+    }).then((carros) => {
+        res.send('Dados do carro atualizado com sucesso!')
+    }).catch((erro) => {
+        res.send('Erro: ' + erro)
+    })
+})
+
+app.get('/deleteCarros', (req, res) => {
+
+    let idCarro = req.query.id
+
+    informacoes.destroy({
+        where: {
+            id: idCarro
+        }
+    }).then(() => {
+        res.send('Dados do carro excluído com sucesso!')
+    }).catch((erro) => {
+        res.send('Erro: ' + erro)
+    })
+})
+
 
 app.listen(3000, console.log('o servidor está rodando'))
